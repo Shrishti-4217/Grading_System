f6d502a0eb46f7493570b7e7230ed1bcf473e9d8
Shrishti-4217 <shrishtimishra437@gmail.com>
2025-07-24 15:11:18 +0530

    Update 24U030072_Lab1.py
    Using a return Function

--- 24U030072_Lab1.py
+++ 24U030072_Lab1.py
@@ -1,18 +1,22 @@
-
+def grade(marks):  #function that takes input as marks and returns the grade
+    if marks<=100 and marks>=90:
+        return "A"
+    elif marks<=89 and marks>=75:
+        return "B"
+    elif marks<=74 and marks>=60:
+        return "C"
+    elif marks<=59 and marks>=40:
+        return "D"
+    elif marks<40:
+        return "F"
+    else:
+        return "Invalid Input"
+    
 ans="YES"
 while(ans!="EXIT"):
     marks=int(input("Enter the marks"))   #give the input of your marks
-    if marks<=100 and marks>=90:
-        print("A\n")
-    elif marks<=89 and marks>=75:
-        print("B\n")
-    elif marks<=74 and marks>=60:
-        print("C\n")
-    elif marks<=59 and marks>=40:
-        print("D\n")
-    elif marks<40:
-        print("F\n")
-    else:
-        print("Invalid Input")   #invalid input
-        continue
+    
+    gd=grade(marks)  #function called to give the grade
+    print("Your Grade is",gd,"\n")
+
     ans=input("Would you like to continue==>")  # type EXIT to stop otherwise type yes
